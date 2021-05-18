f23aa3a50d20227061f3f69263ef768a7a4d5e62
Victor Tennekes <victor@tennekes.nl>
2021-05-18 17:57:12 +0200

    Feat: added notes on requirements

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,16 @@
 #include "../includes/plebserv.hpp"
 
+#include <iostream>
+
 int main(int argc, char **argv, char **env)
 {
-	(void)argc;
+	std::string filename;
+	if (argc == 1)
+	{
+		// get default
+	}
+	else
+		filename = argv[1];
 	(void)argv;
 	(void)env;
 	return (0);
