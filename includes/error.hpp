5ba74192e631cc2bde41e753879d7fb00a5a7db6
Victor Tennekes <victor@tennekes.nl>
2021-05-20 16:31:27 +0200

    Feat: added yellow to colors

--- includes/error.hpp
+++ includes/error.hpp
@@ -3,9 +3,10 @@
 
 # include <string>
 
-# define COLOR_RED "\033[31;01m"
-# define COLOR_GREEN "\033[32m"
-# define COLOR_RESET "\033[0m"
+# define COLOR_RED		"\033[31;01m"
+# define COLOR_GREEN	"\033[32m"
+# define COLOR_RESET	"\033[0m"
+# define COLOR_YELLOW	"\033[33m"
 
 enum ErrorCode
 {
