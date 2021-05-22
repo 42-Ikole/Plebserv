7c7ae0e383c02df47ba72917148a007189691597
Alpha1337k <ozziekruithof@gmail.com>
2021-05-22 20:42:29 +0200

    Chore: moved ends_with

--- includes/utilities.hpp
+++ includes/utilities.hpp
@@ -8,6 +8,7 @@ namespace ft
 	std::vector<std::string> split(std::string str, std::string delim = " \t\n");
 	std::string trim_char(std::string str, char delim);
 	int stoi(std::string number, const std::string base = "0123456789");
+	bool ends_with(std::string const & value, std::string const & ending);
 }
 
 #endif
