732cdda60fff4651b260b3231df0c162e17c50c6
Ingmar Kole <ikole@student.codam.nl>
2021-05-22 11:02:08 +0200

    Chore: translated http status codes

--- src/server_response.cpp
+++ src/server_response.cpp
@@ -15,58 +15,71 @@
 static map<int, string> create_map()
 {
 	map<int, string> m;
-	m[100] = "Doorgaan";
-	m[101] = "Protocolwissel";
-	m[102] = "Processing";
+/*
+**	Informational
+*/
+	m[100] = "Continue";
+	m[101] = "Switching Protocols";
+	m[102] = "Processing (WebDAV)";
 
+/*
+**	SUCCESS
+*/
 	m[200] = "OK";
-	m[201] = "Aangemaakt";
-	m[202] = "Aanvaard";
-	m[203] = "Niet-gemachtigde informatie";
-	m[204] = "Geen inhoud";
-	m[205] = "Inhoud opnieuw instellen";
-	m[206] = "Gedeeltelijke inhoud";
-	m[207] = "Meerdere statussen";
+	m[201] = "Created";
+	m[202] = "Accepted";
+	m[203] = "Non-Authoritative Information";
+	m[204] = "No Content";
+	m[205] = "Reset Content";
+	m[206] = "Partial Content";
+	m[207] = "Multi-Status";
 
-	m[300] = "Meerkeuze";
-	m[301] = "Definitief verplaatst";
-	m[302] = "Tijdelijk verplaatst";
-	m[303] = "Zie andere";
-	m[304] = "Niet gewijzigd";
-	m[305] = "Gebruik Proxy";
-	m[306] = "(Gereserveerd)";
-	m[307] = "Tijdelijke omleiding";
-	m[308] = "Definitieve omleiding";
+/*
+**	Redirection
+*/
+	m[300] = "Multiple Choices";
+	m[301] = "Moved Permanently";
+	m[302] = "Found";
+	m[303] = "See Other";
+	m[304] = "Not Modified";
+	m[305] = "Use Proxy";
+	m[306] = "(Unused)";
+	m[307] = "Temporary Redirect";
+	m[308] = "Permanent Redirect (experimental)";
 
-	m[400] = "Foute aanvraag";
-	m[401] = "Niet geautoriseerd";
-	m[402] = "Betaalde toegang";
-	m[403] = "Verboden toegang";
-	m[404] = "Niet gevonden";
-	m[405] = "Methode niet toegestaan";
-	m[406] = "Niet aanvaardbaar";
-	m[407] = "Authenticatie op de proxyserver verplicht";
-	m[408] = "Aanvraagtijd verstreken";
+/*
+**	Client error
+*/
+	m[400] = "Bad Request";
+	m[401] = "Unauthorized";
+	m[402] = "Payment Required";
+	m[403] = "Forbidden";
+	m[404] = "Not Found";
+	m[405] = "Method Not Allowed";
+	m[406] = "Not Acceptable";
+	m[407] = "Proxy Authentication Required";
+	m[408] = "Request Timeout";
 	m[409] = "Conflict";
-	m[410] = "Verdwenen";
-	m[411] = "Lengte benodigd";
-	m[412] = "Niet voldaan aan vooraf gestelde voorwaarde";
-	m[413] = "Aanvraag te groot";
-	m[414] = "Aanvraag-URL te lang";
-	m[415] = "Media-type niet ondersteund";
-	m[416] = "Aangevraagd gedeelte niet opvraagbaar";
-	m[417] = "Niet voldaan aan verwachting";
+	m[410] = "Gone";
+	m[411] = "Length Required";
+	m[412] = "Precondition Failed";
+	m[413] = "Request Entity Too Large";
+	m[414] = "Request-URI Too Long";
+	m[415] = "Unsupported Media Type";
+	m[416] = "Requested Range Not Satisfiable";
+	m[417] = "Expectation Failed";
 	m[418] = "I'm a teapot";
-	m[419] = "Pagina verlopen";
-	m[422] = "Aanvraag kan niet verwerkt worden";
-	m[423] = "Afgesloten";
-	m[424] = "Gefaalde afhankelijkheid";
-	m[426] = "Upgrade nodig";
-	m[428] = "Voorwaarde nodig";
-	m[429] = "Te veel requests";
-	m[431] = "Headers van de aanvraag te lang";
+	m[419] = "Page expired";
+	m[420] = "Spark up baby / enhance your calm";
+	m[422] = "Unprocessable Entity";
+	m[423] = "Locked";
+	m[424] = "Failed Dependency";
+	m[426] = "Upgrade Required";
+	m[428] = "Precondition Required";
+	m[429] = "Too Many Requests";
+	m[431] = "Request Header Fields Too Large";
 	m[450] = "Geblokkeerd door Windows Parental Control";
-	m[451] = "Toegang geweigerd om juridische redenen.";
+	m[451] = "Unavailable For Legal Reasons";
 	m[494] = "Request Header Too Large";
 	m[495] = "Cert Error";
 	m[496] = "No Cert";
@@ -74,17 +87,20 @@ static map<int, string> create_map()
 	m[498] = "Token expired/invalid";
 	m[499] = "Token required";
 
-	m[500] = "Interne serverfout";
-	m[501] = "Niet geïmplementeerd";
+/*
+** Server error
+*/
+	m[500] = "Internal Server Error";
+	m[501] = "Not Implemented";
 	m[502] = "Bad Gateway";
-	m[503] = "Dienst niet beschikbaar";
+	m[503] = "Service Unavailable";
 	m[504] = "Gateway Timeout";
-	m[505] = "HTTP-versie wordt niet ondersteund";
-	m[509] = "Bandbreedte overschreden";
-	m[510] = "Niet verlengd";
-	m[511] = "Netwerkauthenticatie vereist";
-	m[522] = "Connectie duurt te lang";
-	m[525] = "TLS-handshake mislukt";
+	m[505] = "HTTP Version Not Supported";
+	m[509] = "Bandwidth Limit Exceeded";
+	m[510] = "Not Extended";
+	m[511] = "Network Authentication Required";
+	m[522] = "Network read timeout error";
+	m[525] = "Network connect timeout error";
 	return m;
 }
 
